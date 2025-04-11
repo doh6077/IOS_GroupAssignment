e127303bddc47c13b68d1458a0eba0959c659d6a
dohee <kimdohee@sheridancollege.ca>
2025-04-10 20:40:28 -0400

    Complete onboarding screens using UICollectionView and custom cells

---
+++ GroupAssignment/Models/OnboardingSlide.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardingSlide.swift
+//  GroupAssignment
+//
+//  Created by Dohee Kim on 2025-04-10.
+//
+
+import UIKit
+
+struct OnboardingSlide {
+    let title: String
+    let description: String
+    let image: UIImage
+}
